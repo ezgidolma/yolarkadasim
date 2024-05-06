316af45a8e5d6d6e7f7368602217eb41122a55a8
ezgid <ezgidolma23@gmail.com>
2024-05-06 19:09:02 +0300

    atlas veritabanı eklendi.

--- src/main/java/com/example/yolarkadasim/controller/TripController.java
+++ src/main/java/com/example/yolarkadasim/controller/TripController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/seyahatlar")
+@RequestMapping("/api/seyahatler")
 public class TripController {
     @Autowired
     private TripService tripService;
